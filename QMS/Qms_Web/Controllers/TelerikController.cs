3b6bc64512388997070e72d02dfe5362db65009d
LeeTTrent <7557877312@GSA.GOV>
2022-06-21 14:09:02 -0500

    Successfully Tested Kendo Grid

--- QMS/Qms_Web/Controllers/TelerikController.cs
+++ QMS/Qms_Web/Controllers/TelerikController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+using Qms_Web.Models;
 
 namespace Qms_Web.Controllers
 {
@@ -7,6 +10,29 @@ namespace Qms_Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Orders_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = Enumerable.Range(0, 50).Select(i => new TelerikViewModel
+            {
+                OrderID = i,
+                Freight = i * 10,
+                OrderDate = new DateTime(2016, 9, 15).AddDays(i % 7),
+                ShipName = "ShipName " + i,
+                ShipCity = "ShipCity " + i
+            });
+
+            string logSnippet = "[GridController][Orders_Read] =>";
+            Console.WriteLine();
+            foreach (var order in result)
+            {
+                Console.WriteLine($"{logSnippet} (order.ShipName): '{order.ShipName}'");
+            }
+            Console.WriteLine();
+
+            var dsResult = result.ToDataSourceResult(request);
+            return Json(dsResult);
         }
     }
 }
