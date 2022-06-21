8fccaa2c1e8c74d4302fe506feab180886b4d320
LeeTTrent <7557877312@GSA.GOV>
2022-06-21 12:06:26 -0500

    Setup Telerik server-side and client-side dependencies

--- QMS/Qms_Web/Program.cs
+++ QMS/Qms_Web/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Added server-side Kendo
+builder.Services.AddKendo();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
