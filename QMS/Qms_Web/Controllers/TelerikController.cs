0a4078a7117d8292ebf26fd5e6de87226d497d48
LeeTTrent <7557877312@GSA.GOV>
2022-06-22 10:00:53 -0500

    Moved TelerikViewModel into TelerikController

--- QMS/Qms_Web/Controllers/TelerikController.cs
+++ QMS/Qms_Web/Controllers/TelerikController.cs
@@ -341,4 +341,38 @@ namespace Qms_Web.Controllers
         }
 
     }
+
+    public class TelerikViewModel
+    {
+        public int OrderID
+        {
+            get;
+            set;
+        }
+
+        public decimal? Freight
+        {
+            get;
+            set;
+        }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public DateTime? OrderDate
+        {
+            get;
+            set;
+        }
+
+        public string ShipCity
+        {
+            get;
+            set;
+        }
+
+        public string ShipName
+        {
+            get;
+            set;
+        }
+    }
 }
