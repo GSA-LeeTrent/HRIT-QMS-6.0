58c5237e0c910ea525466c58892e1ec4c2290996
LeeTTrent <7557877312@GSA.GOV>
2022-06-22 10:10:37 -0500

    Moved QmsChart into TelerikController

--- QMS/Qms_Web/Controllers/TelerikController.cs
+++ QMS/Qms_Web/Controllers/TelerikController.cs
@@ -375,4 +375,29 @@ namespace Qms_Web.Controllers
             set;
         }
     }
+
+    public class QMSChart
+    {
+        public QMSChart(int id, string key, int value)
+        {
+            Id = id;
+            Key = key;
+            Value = value;
+        }
+
+        public QMSChart(int id, string key, int val1, int val2, int val3)
+        {
+            Id = id;
+            Key = key;
+            Value = val1;
+            ValueTwo = val2;
+            ValueThree = val3;
+        }
+
+        public int Id { get; set; }
+        public string Key { get; set; } = string.Empty;
+        public int Value { get; set; }
+        public int ValueTwo { get; set; }
+        public int ValueThree { get; set; }
+    }
 }
