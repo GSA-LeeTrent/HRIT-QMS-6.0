b4b2380b1200483d3ed856fda60697f5a0f1a53c
LeeTTrent <7557877312@GSA.GOV>
2022-07-05 12:31:46 -0500

    Added QMS menu tester page

---
+++ QMS/Qms_Web/Controllers/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Qms_Web.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
