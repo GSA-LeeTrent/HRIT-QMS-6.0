55d5c8e7ddc604b68b714d93e60eec4547515387
LeeTTrent <7557877312@GSA.GOV>
2022-07-07 13:08:45 -0500

    Completed development of QMS Menu using Telerik UI

--- QMS/Qms_Web/Controllers/MenuController.cs
+++ QMS/Qms_Web/Controllers/MenuController.cs
@@ -5,7 +5,6 @@ using Qms_Web.Constants;
 using Qms_Web.Extensions;
 using QmsCore.Services;
 using QmsCore.UIModel;
-using System.Text;
 
 namespace Qms_Web.Controllers
 {
@@ -19,15 +18,10 @@ namespace Qms_Web.Controllers
             _menuBuilderService = menuBuilderService;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult Menu_Read([DataSourceRequest] DataSourceRequest request)
         {
             List<ModuleMenuItem> parentMenus = HttpContext.Session.GetObject<List<ModuleMenuItem>>(QmsConstants.MODULE_MENU_ITEMS_SESSION_KEY)!;
-            if ( parentMenus == null)
+            if (parentMenus == null)
             {
                 return View(new List<ModuleMenuItem>());
             }
@@ -37,14 +31,14 @@ namespace Qms_Web.Controllers
                 List<QmsCore.UIModel.MenuItem> childMenus = parentMenu.MenuItems;
                 foreach (var childMenu in childMenus)
                 {
-                    if (childMenu.Controller.Equals("User2"))       {childMenu.Controller = "UserAdmin";}
-                    if (childMenu.Controller.Equals("Role"))        {childMenu.Controller = "RoleAdmin";}
-                    if (childMenu.Controller.Equals("Permission"))  {childMenu.Controller = "PermissionAdmin";}
+                    if (childMenu.Controller.Equals("User2")) { childMenu.Controller = "UserAdmin"; }
+                    if (childMenu.Controller.Equals("Role")) { childMenu.Controller = "RoleAdmin"; }
+                    if (childMenu.Controller.Equals("Permission")) { childMenu.Controller = "PermissionAdmin"; }
 
                     if (string.IsNullOrEmpty(childMenu.UseCase) == false
                             && string.IsNullOrWhiteSpace(childMenu.UseCase) == false)
                     {
-                        childMenu.MenuUrlField = Url.Action(childMenu.ControllerAction, childMenu.Controller, new { UseCase=childMenu.UseCase });
+                        childMenu.MenuUrlField = Url.Action(childMenu.ControllerAction, childMenu.Controller, new { UseCase = childMenu.UseCase });
                     }
                     else
                     {
@@ -52,7 +46,7 @@ namespace Qms_Web.Controllers
                     }
                 }
             }
- 
+
             //foreach (var parentMenu in parentMenus)
             //{
             //    System.Console.WriteLine();
