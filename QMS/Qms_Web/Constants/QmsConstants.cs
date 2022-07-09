320fdda864b48762be3245f95c842db913e288ee
Lee Trent <lee.trent@gsa.gov>
2022-07-09 09:45:06 -0500

    Secure auth dev (#7)
    * Got SecureAuth working out on dev
    
    * Fixed redirect bug
    
    * Added timestamp to home index page
    
    Co-authored-by: LeeTTrent <7557877312@GSA.GOV>

--- QMS/Qms_Web/Constants/QmsConstants.cs
+++ QMS/Qms_Web/Constants/QmsConstants.cs
@@ -2,8 +2,20 @@
 {
     public static class QmsConstants
     {
+        // QMS User
         public static readonly string USER_SESSION_KEY = "QmsUserSessionKey";
+        public static readonly string USER_EMAIL_ADDRESS = "QmsUserEmailAddress";
 
+        // QMS Menu
         public static readonly string MODULE_MENU_ITEMS_SESSION_KEY = "ModuleMenuItemSessionKey";
+
+        // Secure Auth
+        public static readonly string ENCRYPTION_COMPATABILITY_MODE = "EncryptionCompatabilityMode";
+        public static readonly string FRAMEWORK45 = "Framework45";
+        public static readonly string FRAMEWORK20SP2 = "Framework20SP2";
+
+
+        public static readonly string REQUESTED_URI = "requestedUri";
+
     }
 }
