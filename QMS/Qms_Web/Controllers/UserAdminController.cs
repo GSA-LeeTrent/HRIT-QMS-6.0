7e7ad79eaa94a0c3d2d595378836fd06cba28d99
LeeTTrent <7557877312@GSA.GOV>
2022-07-10 10:06:13 -0500

    Using partial views for user admin index page

--- QMS/Qms_Web/Controllers/UserAdminController.cs
+++ QMS/Qms_Web/Controllers/UserAdminController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QmsCore.Services;
 
 namespace Qms_Web.Controllers
 {
     public class UserAdminController : Controller
     {
+        private readonly IUserService _userService;
+
+        public UserAdminController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,13 +20,13 @@ namespace Qms_Web.Controllers
         public IActionResult _ActiveUsers()
         {
             Console.WriteLine("\n[UserAdminController][ActiveUsers] =>\n");
-            return PartialView();
+            return PartialView(_userService.RetrieveActiveUsers());
         }
 
         public IActionResult _InactiveUsers()
         {
             Console.WriteLine("\n[UserAdminController][InactiveUsers] =>\n");
-            return PartialView();
+            return PartialView(_userService.RetrieveInactiveUsers());
         }
     }
 }
