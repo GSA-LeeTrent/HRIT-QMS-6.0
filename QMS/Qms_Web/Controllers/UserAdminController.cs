3d6783bcacdd13d13a2177c403954f2761bf97c7
LeeTTrent <7557877312@GSA.GOV>
2022-07-10 10:46:12 -0500

    Displays active tab when user admin index page first renders

--- QMS/Qms_Web/Controllers/UserAdminController.cs
+++ QMS/Qms_Web/Controllers/UserAdminController.cs
@@ -14,18 +14,29 @@ namespace Qms_Web.Controllers
 
         public IActionResult Index()
         {
+            ViewData["ShowUserAdminActiveTab"] = "True";
+            ViewData["ShowUserAdmiInactiveTab"] = "False";
+
             return View();
         }
 
         public IActionResult _ActiveUsers()
         {
             Console.WriteLine("\n[UserAdminController][ActiveUsers] =>\n");
+
+            //ViewData["ShowUserAdminActiveTab"] = "true"; 
+            //ViewData["ShowUserAdmiInactiveTab"] = "false";
+
             return PartialView(_userService.RetrieveActiveUsers());
         }
 
         public IActionResult _InactiveUsers()
         {
             Console.WriteLine("\n[UserAdminController][InactiveUsers] =>\n");
+
+            //ViewData["ShowUserAdminActiveTab"] = "false";
+            //ViewData["ShowUserAdmiInactiveTab"] = "true"; 
+
             return PartialView(_userService.RetrieveInactiveUsers());
         }
     }
