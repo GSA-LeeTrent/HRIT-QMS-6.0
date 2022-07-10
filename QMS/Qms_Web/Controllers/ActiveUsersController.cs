5479d2d215aa1eb03f57e5925c152eb2747df1c2
LeeTTrent <7557877312@GSA.GOV>
2022-07-10 13:20:46 -0500

    Got pagination working for user admin - active users

---
+++ QMS/Qms_Web/Controllers/ActiveUsersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using QmsCore.Services;
+using QmsCore.UIModel;
+using Qms_Web.Models;
+
+namespace Qms_Web.Controllers
+{
+    public class ActiveUsersController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public ActiveUsersController(IUserService userService)
+        {
+            Console.WriteLine("\n[ActiveUsersController][Constructor] =>");
+            _userService = userService;
+        }
+
+        public ActionResult Paging()
+        {
+            Console.WriteLine("\n[ActiveUsersController][Paging][GET] =>");
+            return View(new PagerViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Paging(PagerViewModel pager)
+        {
+            Console.WriteLine("\n[ActiveUsersController][Paging][POST] =>");
+            return View(pager);
+        }
+
+        public ActionResult ActiveUsers_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            Console.WriteLine("\n[ActiveUsersController][ActiveUsers_Read] =>");
+
+            List<User> activeUsers = _userService.RetrieveActiveUsers();
+            var dsResult = activeUsers.ToDataSourceResult(request);
+            return Json(dsResult);
+        }
+    }
+}
