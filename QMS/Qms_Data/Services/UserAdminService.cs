64ff4f1ae87960b82cfc9446201076783743b9de
Lee Trent <lee.trent@gsa.gov>
2022-07-17 12:16:42 -0500

    User admin list page (#9)
    * Refactored user admin service and repository
    
    * Added mapToViewModel private method to UserAdminService
    
    Co-authored-by: LeeTTrent <7557877312@GSA.GOV>

---
+++ QMS/Qms_Data/Services/UserAdminService.cs
@@ -0,0 +1,46 @@
+﻿using Qms_Data.Repositories;
+using Qms_Data.Repositories.Interfaces;
+using Qms_Data.Services.Interfaces;
+using Qms_Data.ViewModels;
+using QmsCore.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Qms_Data.Services
+{
+    public class UserAdminService : IUserAdminService
+    {
+        private readonly IUserAdminRepository _repository;
+
+        public UserAdminService()
+        {
+            _repository = new UserAdminRepository();
+        }
+
+        public IList<UserListRowVM> RetrieveActiveUsers()
+        {
+            return this.mapToViewModel(_repository.RetrieveActiveUsers());
+        }
+
+        public IList<UserListRowVM> RetrieveInactiveUsers()
+        {
+            return this.mapToViewModel(_repository.RetrieveInactiveUsers());
+        }
+
+        private IList<UserListRowVM> mapToViewModel(IQueryable<SecUser> entities)
+        {
+            IList<UserListRowVM> viewModels = new List<UserListRowVM>();
+            foreach (SecUser entity in entities)
+            {
+                viewModels.Add(new UserListRowVM(
+                    (int)entity.UserId,
+                    entity.EmailAddress,
+                    entity.DisplayName,
+                    (entity.Org == null) ? "" : entity.Org.OrgLabel,
+                    (entity.Manager == null) ? "" : entity.Manager.DisplayName)
+                );
+            }
+            return viewModels;
+        }
+    }
+}
---
+++ QMS/Qms_Data/ViewModels/UserListRowVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qms_Data.ViewModels
+{
+    public class UserListRowVM
+    {
+        public UserListRowVM(int userId, string emailAddress, string displayName, string organizationName, string managerName)
+        {
+            UserId = userId;
+            EmailAddress = emailAddress;
+            DisplayName = displayName;
+            OrganizationName = organizationName;
+            ManagerName = managerName;
+        }
+
+        public int UserId { get; }
+        public string EmailAddress { get; }
+        public string DisplayName { get; }
+        public string OrganizationName { get; }
+        public string ManagerName { get;  }
+    }
+}
