073c43038de0d45489d870726cf9fc0c5d0b4a25
Lee Trent <lee.trent@gsa.gov>
2022-07-17 08:31:14 -0500

    User Admin Dev (#8)
    * Added branch name to home index page
    
    * Just getting started on user admin
    
    * Working on user admin index page and partial views
    
    * Using partial views for user admin index page
    
    * Displays active tab when user admin index page first renders
    
    * Got pagination working for user admin - active users
    
    * Got pagination working with user admin active and inactive grids
    
    * Only sys admins can access user admin pages
    
    * Added new user button to active tab in user admin index page
    
    * Changed style of add new user button to primary
    
    * Working on modal popup
    
    * Working on modal popup
    
    * Using kendo button to call bootstrap modal window
    
    * Added toolbar to active users grid
    
    * Added QMS logo
    
    * Working on page nav bar
    
    * Working on logout button
    
    * Changed to bootstrap nordic theme
    
    * Changed styling of submit button on login page
    
    Co-authored-by: LeeTTrent <7557877312@GSA.GOV>

--- QMS/Qms_Web/Controllers/UserAdminController.cs
+++ QMS/Qms_Web/Controllers/UserAdminController.cs
@@ -1,12 +1,87 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using QmsCore.Services;
+using QmsCore.UIModel;
+using Qms_Web.Models;
 
 namespace Qms_Web.Controllers
 {
+    [Authorize(Roles = "SYS_ADMIN")]
     public class UserAdminController : Controller
     {
+        private readonly IUserService _userService;
+
+        public UserAdminController(IUserService userService)
+        {
+            Console.WriteLine("\n[UserAdminController][Constructor] =>");
+            _userService = userService;
+        }
+
         public IActionResult Index()
         {
+            Console.WriteLine("\n[UserAdminController][Index] =>");
+
+            ViewData["ShowUserAdminActiveTab"] = "True";
+            ViewData["ShowUserAdmiInactiveTab"] = "False";
+
             return View();
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////
+        // ACTIVE USERS
+        ////////////////////////////////////////////////////////////////////////////////
+
+        [HttpGet]
+        public IActionResult _ActiveUsers()
+        {
+            Console.WriteLine("\n[UserAdminController][_ActiveUsers][GET] =>\n");
+            return PartialView(new PagerViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult _ActiveUsers(PagerViewModel pager)
+        {
+            Console.WriteLine("\n[ActiveUsersController][_ActiveUsers][POST] =>");
+            return PartialView(pager);
+        }
+
+        public ActionResult ActiveUsers_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            Console.WriteLine("\n[UserAdminController][ActiveUsers_Read] =>");
+
+            List<User> activeUsers = _userService.RetrieveActiveUsers();
+            var dsResult = activeUsers.ToDataSourceResult(request);
+            return Json(dsResult);
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////
+        // INACTIVE USERS
+        ////////////////////////////////////////////////////////////////////////////////
+
+        [HttpGet]
+        public IActionResult _InactiveUsers()
+        {
+            Console.WriteLine("\n[UserAdminController][InactiveUsers][GET] =>\n");
+
+            return PartialView(new PagerViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult _InactiveUsers(PagerViewModel pager)
+        {
+            Console.WriteLine("\n[UserAdminController][InactiveUsers][POST] =>");
+            return PartialView(pager);
+        }
+
+        public ActionResult InactiveUsers_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            Console.WriteLine("\n[UserAdminController][InactiveUsers_Read] =>");
+
+            List<User> inactiveUsers = _userService.RetrieveInactiveUsers();
+            var dsResult = inactiveUsers.ToDataSourceResult(request);
+            return Json(dsResult);
         }
     }
 }
