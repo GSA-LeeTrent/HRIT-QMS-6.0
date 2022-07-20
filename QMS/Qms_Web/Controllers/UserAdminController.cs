65e5b3b60e5f6d6f33025c99c263636bc77e00c5
LeeTTrent <7557877312@GSA.GOV>
2022-07-20 15:45:06 -0500

    Added helper methods to organization and manager dropdowns and role checkboxes for Create/Update User

--- QMS/Qms_Web/Controllers/UserAdminController.cs
+++ QMS/Qms_Web/Controllers/UserAdminController.cs
@@ -110,7 +110,7 @@ namespace Qms_Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult CreateUser([Bind("ManagerId, OrgId, EmailAddress, DisplayName")]  UserAdminFormVM userFormVM, string[] selectedRoleIdsForUser)
+        public IActionResult CreateUser([Bind("ManagerId, OrgId, EmailAddress, DisplayName")]  UserAdminFormVM uaFormVM, string[] selectedRoleIdsForUser)
         {
             ///////////////////////////////////////////////////////////////////////////////////
             // User's Role Selection Validation
@@ -118,10 +118,57 @@ namespace Qms_Web.Controllers
             if (selectedRoleIdsForUser == null || selectedRoleIdsForUser.Length == 0)
             {
                 ModelState.AddModelError(string.Empty, "Please select at least one role.");
-                return View(userFormVM);
             }
-            int newUserId = _userAdminService.CreateUser(userFormVM, selectedRoleIdsForUser);
-            return RedirectToAction(nameof(Index));
+
+            if (ModelState.IsValid)
+            {
+                int newUserId = _userAdminService.CreateUser(uaFormVM, selectedRoleIdsForUser);
+                return RedirectToAction(nameof(Index));
+            }
+
+            uaFormVM.Mutatatable = true;
+            uaFormVM.Deactivatable = false;
+            uaFormVM.Reactivatable = false;
+            uaFormVM.AspAction = "CreateUser";
+            uaFormVM.SubmitButtonLabel = "Create";
+            uaFormVM.CardHeader = "Add New User:";
+
+            this.populateOrganizationAndManagerDropdowns(uaFormVM);
+            this.populateRoleCheckboxes(uaFormVM);
+
+            return View(uaFormVM);
+        }
+
+        private void populateOrganizationAndManagerDropdowns(UserAdminFormVM uaFormVM)
+        {
+            // ORGANIZATIONS
+            List<Organization> activeOrganizations = _organizationService.RetrieveActiveOrganizations();
+            if (uaFormVM.OrgId.HasValue && uaFormVM.OrgId.Value > 0)
+            {
+                ViewBag.ActiveOrganizations = new SelectList(activeOrganizations, "OrgId", "OrgLabel", uaFormVM.OrgId);
+            }
+            else
+            {
+                ViewBag.ActiveOrganizations = new SelectList(activeOrganizations, "OrgId", "OrgLabel");
+            }
+
+            if (uaFormVM.OrgId.HasValue && uaFormVM.OrgId.Value > 0
+                    && uaFormVM.ManagerId.HasValue && uaFormVM.ManagerId.Value > 0)
+            {
+                int orgId = uaFormVM.OrgId.Value;
+                List<ManagerSelectOptionVM> usersInOrg = _userAdminService.RetrieveUsersByOrgId(orgId);
+                ViewBag.PotentialManagers = new SelectList(usersInOrg, "UserId", "DisplayLabel", uaFormVM.ManagerId);
+            }
+            else
+            {
+                List<User> usersInOrg = new List<User>();
+                ViewBag.PotentialManagers = new SelectList(usersInOrg, "UserId", "DisplayLabel");
+            }
+        }
+
+        private void populateRoleCheckboxes(UserAdminFormVM uaFormVM)
+        {
+            uaFormVM.CheckboxRoles = _userAdminService.RetrieveActiveRoles();
         }
     }
 }
